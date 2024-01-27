cfb0b66f36df0ced00523bef8ed685cbe72f77d9
BA7JCM <1677128936@qq.com>
2024-01-27 15:36:35 +0800

    clean local , upload to cloud

--- Statics/js/copyright.js
+++
@@ -1,4 +0,0 @@
-// dynamically update the copyright year
-// 动态更新版权年份
-var currentYear = new Date().getFullYear();
-document.getElementById("copyright").innerText = "2021-" + currentYear + " Gaoice Team";
